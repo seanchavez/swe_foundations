e55d361080c1c523914dbc2914cf25b22fb647cd
seanchavez <realseanchavez@gmail.com>
2021-06-04 01:42:23 -0700

    Complete 03_bubble_sort.rb

--- algorithmic_strategies_exercise/03_bubble_sort.rb
+++ algorithmic_strategies_exercise/03_bubble_sort.rb
@@ -3,6 +3,21 @@
 # The method should return the array.
 # Do not use the built-in Array#sort
 
+def bubble_sort(arr)
+  sorted = false
+  partition = arr.length - 1
+  until sorted
+    sorted = true
+    (0...partition).each do |i|
+      if arr[i] > arr[i + 1]
+        sorted = false
+        arr[i + 1], arr[i] = arr[i], arr[i + 1]
+      end 
+    end
+    partition -= 1
+  end
+  arr
+end
 
 p bubble_sort([2, 8, 5, 2, 6])      # => [2, 2, 5, 6, 8]
 p bubble_sort([10, 8, 7, 1, 2, 3])  # => [1, 2, 3, 7, 8, 10]
