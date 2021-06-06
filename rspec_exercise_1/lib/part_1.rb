842c2fb121cfe748cf8e74bb89cd334dbe6888c1
seanchavez <realseanchavez@gmail.com>
2021-06-05 21:50:01 -0700

    Complete rspec_exercise_1 part_1.rb

--- rspec_exercise_1/lib/part_1.rb
+++ rspec_exercise_1/lib/part_1.rb
@@ -1,3 +1,21 @@
 def average(m, n)
   (m + n) / 2.0
 end
+
+def average_array(nums)
+  nums.sum / nums.length.to_f
+end
+
+def repeat(str, num)
+  result = ""
+  num.times {result << str}
+  result
+end
+
+def yell(str)
+  str.upcase + "!"
+end
+
+def alternating_case(sentence)
+  sentence.split.map.with_index {|word, i| i.odd? ? word.downcase : word.upcase}.join(" ")
+end
