853f2be7d4d4104bf289f05c8e80a809ab0e4bf3
seanchavez <realseanchavez@gmail.com>
2021-06-07 11:17:19 -0700

    rspec_exercise_2 part1.rb WIP
    Implement partition, merge, and censor.

--- rspec_exercise_2/lib/part_1.rb
+++ rspec_exercise_2/lib/part_1.rb
@@ -1,3 +1,21 @@
 def partition(arr, n)
-  
+  [arr.select {|el| el < n}, arr.select {|el| el >= n}]
+end
+
+def merge(hash_1, hash_2)
+  merged = {}
+  hash_1.each {|k, v| merged[k] = v}
+  hash_2.each {|k, v| merged[k] = v}
+  merged
+end
+
+def censor(sentence, curse_words)
+  vowel_s = "aeiou"
+  sentence.split.map do |word|
+    if curse_words.include?(word.downcase)
+      word.chars.map {|char| vowel_s.include?(char.downcase) ? "*" : char}.join
+    else
+      word
+    end
+  end.join(" ")
 end
