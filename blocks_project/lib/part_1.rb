1ef17cd81ec86ba84f15eee0ef918b297f6cbdb4
seanchavez <realseanchavez@gmail.com>
2021-06-10 15:49:51 -0700

    blocks_project: part1.rb WIP
    Implement reject_puppies
    Implement count_positive_subarrays

--- blocks_project/lib/part_1.rb
+++ blocks_project/lib/part_1.rb
@@ -1,7 +1,13 @@
+require "byebug"
+
 def select_even_nums(nums)
   nums.select(&:even?)
 end
 
 def reject_puppies(dogs)
-  
+  dogs.reject {|dog| dog["age"] <= 2}
+end
+
+def count_positive_subarrays(two_d_arr)
+  two_d_arr.count {|arr| arr.sum > 0}
 end
