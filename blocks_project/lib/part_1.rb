c8a73613b4984a400d54ae2c3f0f929ef668e363
seanchavez <realseanchavez@gmail.com>
2021-06-10 16:39:17 -0700

    blocks_project: Complete part_1.rb
    Implement aba_translate
    Implement aba_array

--- blocks_project/lib/part_1.rb
+++ blocks_project/lib/part_1.rb
@@ -11,3 +11,18 @@ end
 def count_positive_subarrays(two_d_arr)
   two_d_arr.count {|arr| arr.sum > 0}
 end
+
+def aba_translate(word)
+  vowel_s = "aeiou"
+  word.chars.reduce("") do |trans, char| 
+    if vowel_s.include?(char)
+      trans + char + "b" + char
+    else
+      trans + char
+    end
+  end
+end
+
+def aba_array(words)
+  words.map {|word| aba_translate(word)}
+end
