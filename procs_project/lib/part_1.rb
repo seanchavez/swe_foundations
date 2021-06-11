2c962e4c0543d6eb34e14790bf9fcc354f586b16
seanchavez <realseanchavez@gmail.com>
2021-06-11 13:56:07 -0700

    procs_project: Complete part_1.rb
    Implement my_any?
    Implement my_all?
    Implement my_none?

--- procs_project/lib/part_1.rb
+++ procs_project/lib/part_1.rb
@@ -15,3 +15,18 @@ def my_count(arr, &blk)
   arr.each {|el| count += 1 if blk.call(el)}
   count
 end
+
+def my_any?(arr, &blk)
+  arr.each {|el| return true if blk.call(el)}
+  false
+end
+
+def my_all?(arr, &blk)
+  arr.each {|el| return false unless blk.call(el)}
+  true
+end
+
+def my_none?(arr, &blk)
+  arr.each {|el| return false if blk.call(el)}
+  true
+end
