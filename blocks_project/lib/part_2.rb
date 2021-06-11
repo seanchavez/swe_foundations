6a9e8041623049f148674a455f12a6e5ce30c291
seanchavez <realseanchavez@gmail.com>
2021-06-10 18:06:16 -0700

    blocks_project: Complete part_2.rb
    Implement any_passing_students?

--- blocks_project/lib/part_2.rb
+++ blocks_project/lib/part_2.rb
@@ -5,3 +5,7 @@ end
 def no_valid_url?(urls)
   urls.none? {|url| url.end_with?('.com', '.net', '.io', '.org')}
 end
+
+def any_passing_students?(students)
+  students.any? {|student| student[:grades].sum / student[:grades].length >= 75}
+end
