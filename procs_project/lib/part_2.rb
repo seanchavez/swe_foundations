9316c41f9a2a36776b2eb3f209ba156d3758cb1b
seanchavez <realseanchavez@gmail.com>
2021-06-12 18:55:15 -0700

    procs_project: Complete part_2.rb
    Implement and_selector
    "     " alternating_mapper

--- procs_project/lib/part_2.rb
+++ procs_project/lib/part_2.rb
@@ -11,3 +11,11 @@ def greater_proc_value(num, prc_1, prc_2)
   result_2 = prc_2.call(num)
   result_1 > result_2 ? result_1 : result_2
 end
+
+def and_selector(arr, prc_1, prc_2)
+  arr.select {|el| prc_1.call(el) && prc_2.call(el)}
+end
+
+def alternating_mapper(arr, prc_1, prc_2)
+  arr.map.with_index {|el, i| i.even? ? prc_1.call(el) : prc_2.call(el)}
+end
