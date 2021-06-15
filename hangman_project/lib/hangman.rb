b4b1f7c10c00eb36f30d9474bad42a0fbb9e86e5
seanchavez <realseanchavez@gmail.com>
2021-06-15 10:51:24 -0700

    hangman_project: Complete Part 1

--- hangman_project/lib/hangman.rb
+++ hangman_project/lib/hangman.rb
@@ -23,4 +23,18 @@ class Hangman
   def remaining_incorrect_guesses
     @remaining_incorrect_guesses
   end
+
+  def already_attempted?(char)
+    attempted_chars.include?(char)
+  end
+
+  def get_matching_indices(char)
+    matches = []
+    @secret_word.chars.each_with_index {|c, i| matches << i if c == char}
+    matches
+  end
+
+  def fill_indices(char, indices)
+    indices.each {|index| @guess_word[index] = char}
+  end
 end
