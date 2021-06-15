d8df2eb020add733219f0ee93c3806c75d373a24
seanchavez <realseanchavez@gmail.com>
2021-06-15 08:29:49 -0700

    Complete guessing_game_project

--- guessing_game_project/lib/guessing_game.rb
+++ guessing_game_project/lib/guessing_game.rb
@@ -1,3 +1,32 @@
 class GuessingGame
+  def initialize(min, max)
+    @secret_num = rand(min..max)
+    @num_attempts = 0
+    @game_over = false
+  end
 
+  def num_attempts
+    @num_attempts
+  end
+
+  def game_over?
+    @game_over
+  end
+
+  def check_num(num)
+    @num_attempts += 1
+    case  
+    when num > @secret_num then puts "too big"
+    when num < @secret_num then puts "too small"
+    else   
+      @game_over = true 
+       puts "you win"
+    end
+  end
+
+  def ask_user
+    puts "Enter a number: "
+    input_num = gets.chomp.to_i
+    self.check_num(input_num)
+  end
 end
