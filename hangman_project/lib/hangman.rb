9751d8615e9e009440252e0d88f70f9c9f9f6313
seanchavez <realseanchavez@gmail.com>
2021-06-15 09:33:33 -0700

    Init hangman_project: Part 1 WIP

---
+++ hangman_project/lib/hangman.rb
@@ -0,0 +1,26 @@
+class Hangman
+  DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
+
+  def self.random_word
+    DICTIONARY.sample
+  end
+
+  def initialize
+    @secret_word = Hangman.random_word
+    @guess_word = Array.new(@secret_word.length, "_")
+    @attempted_chars = []
+    @remaining_incorrect_guesses = 5
+  end
+
+  def guess_word
+    @guess_word
+  end
+
+  def attempted_chars
+    @attempted_chars
+  end
+
+  def remaining_incorrect_guesses
+    @remaining_incorrect_guesses
+  end
+end
