7421b5eab8291e0bfb372fec225c55d22f07619e
seanchavez <realseanchavez@gmail.com>
2021-06-16 08:55:18 -0700

    hotel_project: Hotel class WIP

--- hotel_project/lib/hotel.rb
+++ hotel_project/lib/hotel.rb
@@ -1,5 +1,25 @@
 require_relative "room"
 
 class Hotel
-  
+  def initialize(name, room_caps)
+    @name = name
+    @rooms = {}
+    room_caps.each {|room, cap| @rooms[room] = Room.new(cap)}
+  end
+
+  def name
+    @name.split.map(&:capitalize).join(" ")
+  end
+
+  def rooms
+    @rooms
+  end
+
+  def room_exists?(rm_name)
+    @rooms.has_key?(rm_name)
+  end
+
+  def check_in(person, rm_name)
+    
+  end
 end
