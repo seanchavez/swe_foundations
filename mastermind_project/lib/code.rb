5891e8e4726739c7bfafad656f3808011466a7b6
seanchavez <realseanchavez@gmail.com>
2021-06-17 10:06:10 -0700

    Init mastermind_project: Begin Part 1

---
+++ mastermind_project/lib/code.rb
@@ -0,0 +1,12 @@
+class Code
+  POSSIBLE_PEGS = {
+    "R" => :red,
+    "G" => :green,
+    "B" => :blue,
+    "Y" => :yellow
+  }
+
+  def self.valid_pegs?(chars)
+    chars.all? {|char| POSSIBLE_PEGS.include?(char.upcase)}
+  end
+end
