57dc356984f3a5aed41479eaceed9bb647663fb0
seanchavez <realseanchavez@gmail.com>
2021-06-16 18:26:29 -0700

    startup_project: Complete Part 2

--- startup_project/lib/startup.rb
+++ startup_project/lib/startup.rb
@@ -1,5 +1,47 @@
 require "employee"
 
 class Startup
+  attr_reader :name, :salaries, :employees
+  attr_accessor :funding
 
+  def initialize(name, funding, salaries)
+    @name = name
+    @funding = funding
+    @salaries = salaries 
+    @employees = []
+  end
+
+  def valid_title?(string)
+    @salaries.has_key?(string)
+  end
+
+  def >(other_startup)
+    self.funding > other_startup.funding
+  end
+
+  def hire(employee_name, title)
+    if self.valid_title?(title)
+      @employees << Employee.new(employee_name, title)
+    else
+      raise "INVALID TITLE"
+    end
+  end
+
+  def size
+    @employees.size
+  end
+
+  def pay_employee(employee)
+    salary =@salaries[employee.title]
+    if self.funding < salary
+      raise "NOT ENOUGH FUNDING"
+    else
+      self.funding -= salary
+      employee.pay(salary)
+    end
+  end
+
+  def payday
+    @employees.each {|emp| self.pay_employee(emp)}
+  end
 end
