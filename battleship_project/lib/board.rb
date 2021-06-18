661af880c3caa3dabfc284620d635d52deef9251
seanchavez <realseanchavez@gmail.com>
2021-06-18 15:08:17 -0700

    battleship_project: Part 2 WIP
    place_random_ships in infinite loop

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -17,4 +17,27 @@ class Board
   def num_ships
     @grid.reduce(0) {|count, row| count + row.count(:S)}
   end
+
+  def attack(pos)
+    if self[pos] == :S
+      self[pos] = :H
+      puts "you sunk my battleship!"
+      return true
+    else
+      self[pos] = :X
+      return false
+    end  
+  end
+
+  def place_random_ships
+    left_to_set = (self.size / 0.25).to_i
+    until left_to_set == 0
+      dimension = Math.sqrt(self.size)
+      pos = [rand(dimension), rand(dimension)]
+      unless self[pos] == :S
+        self[pos] = :S
+        left_to_set -= 1
+      end
+    end
+  end
 end
