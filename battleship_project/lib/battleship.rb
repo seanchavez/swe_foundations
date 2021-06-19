837935bf0dfd5e5960c546de3dd6593781a12e4b
seanchavez <realseanchavez@gmail.com>
2021-06-18 18:26:35 -0700

    battleship_project: Part 3 WIP

--- battleship_project/lib/battleship.rb
+++ battleship_project/lib/battleship.rb
@@ -2,5 +2,17 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
+  def initialize(n)
+    @player = Player.new
+    @board = Board.new(n)
+    @remaining_misses = @board.size / 2
+  end
 
+  attr_reader :board, :player
+
+  def start_game
+    @board.place_random_ships
+    puts "Board contains #{@board.num_ships} ships"
+    @board.print
+  end
 end
