05a7ae807f8a1f6cb9e67137b07eac05476c169e
seanchavez <realseanchavez@gmail.com>
2021-06-30 21:40:29 -0700

    perilous_procs: Phase 1 WIP

---
+++ perilous_procs/phase_1.rb
@@ -0,0 +1,42 @@
+def some?(arr, &blk)
+  arr.each {|el| return true if blk.call(el)}
+  false
+end
+
+p some?([3, 1, 11, 5]) { |n| n.even? }                                # false
+p some?([3, 4, 11, 5]) { |n| n.even? }                                # true
+p some?([8, 2]) { |n| n.even? }                                       # true
+p some?(['squash', 'corn', 'kale', 'carrot']) { |str| str[0] == 'p' } # false
+p some?(['squash', 'corn', 'kale', 'potato']) { |str| str[0] == 'p' } # true
+p some?(['parsnip', 'lettuce', 'pea']) { |str| str[0] == 'p' }        # true
+p "------------"
+
+def exactly?(arr, n, &blk)
+  count = 0
+  arr.each {|el| count += 1 if blk.call(el)}
+  count == n
+end
+
+p exactly?(['A', 'b', 'C'], 2) { |el| el == el.upcase }         # true
+p exactly?(['A', 'B', 'C'], 2) { |el| el == el.upcase }         # false
+p exactly?(['A', 'B', 'C'], 3) { |el| el == el.upcase }         # true
+p exactly?(['cat', 'DOG', 'bird'], 1) { |el| el == el.upcase }  # true
+p exactly?(['cat', 'DOG', 'bird'], 0) { |el| el == el.upcase }  # false
+p exactly?(['cat', 'dog', 'bird'], 0) { |el| el == el.upcase }  # true
+p exactly?([4, 5], 3) { |n| n > 0 }                             # false
+p exactly?([4, 5, 2], 3) { |n| n > 0 }                          # true
+p exactly?([42, -9, 7, -3, -6], 2) { |n| n > 0 }                # true
+p "------------"
+
+
+def filter_out(arr, &blk)
+  new_arr = []
+  arr.each {|el| new_arr << el unless blk.call(el)}
+  new_arr
+end
+
+p filter_out([10, 6, 3, 2, 5 ]) { |x| x.odd? }      # [10, 6, 2]
+p filter_out([1, 7, 3, 5 ]) { |x| x.odd? }          # []
+p filter_out([10, 6, 3, 2, 5 ]) { |x| x.even? }     # [3, 5]
+p filter_out([1, 7, 3, 5 ]) { |x| x.even? }         # [1, 7, 3, 5]
+p "------------"
