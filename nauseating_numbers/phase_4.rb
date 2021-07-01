9a83712c1043adc8b93ea4cd28a27ca1f390f4d0
seanchavez <realseanchavez@gmail.com>
2021-06-30 17:17:09 -0700

    Complete nauseating_numbers
    Implement pretentious_primes

--- nauseating_numbers/phase_4.rb
+++ nauseating_numbers/phase_4.rb
@@ -1,3 +1,5 @@
+require_relative "../rspec_exercise_3/lib/part_1.rb"
+
 def mersenne_prime(n)
   count = 0
   pow = 1
@@ -7,11 +9,6 @@ def mersenne_prime(n)
     count += 1 if is_prime?(poss)
   end
   poss
-end
-
-def is_prime?(num)
-  return false if num < 2
-  (2..num / 2).none? {|n| num % n == 0}
 end
 
 p mersenne_prime(1) # 3
@@ -72,3 +69,26 @@ p consecutive_collapse([1, 9, 8, 6, 4, 5, 7, 9, 2])   # [1, 9, 2]
 p consecutive_collapse([3, 5, 6, 2, 1])               # [1]
 p consecutive_collapse([5, 7, 9, 9])                  # [5, 7, 9, 9]
 p consecutive_collapse([13, 11, 12, 12])              # []
+
+
+def pretentious_primes(nums, n)
+  nums.map do |num|
+    if n.negative?
+      lesser_primes = prime_range(2, num - 1)
+      -n > lesser_primes.length ? nil : lesser_primes[n]
+    else
+      nth_prime(n, num)
+    end
+  end
+end
+
+p pretentious_primes([4, 15, 7], 1)           # [5, 17, 11]
+p pretentious_primes([4, 15, 7], 2)           # [7, 19, 13]
+p pretentious_primes([12, 11, 14, 15, 7], 1)  # [13, 13, 17, 17, 11]
+p pretentious_primes([12, 11, 14, 15, 7], 3)  # [19, 19, 23, 23, 17]
+p pretentious_primes([4, 15, 7], -1)          # [3, 13, 5]
+p pretentious_primes([4, 15, 7], -2)          # [2, 11, 3]
+p pretentious_primes([2, 11, 21], -1)         # [nil, 7, 19]
+p pretentious_primes([32, 5, 11], -3)         # [23, nil, 3]
+p pretentious_primes([32, 5, 11], -4)         # [19, nil, 2]
+p pretentious_primes([32, 5, 11], -5)         # [17, nil, nil]
