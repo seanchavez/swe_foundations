7db5e8b062dc4b898a4d2cd772caf54b5b8ac411
seanchavez <realseanchavez@gmail.com>
2021-07-01 10:25:09 -0700

    perilous_procs: Phase 2 WIP

---
+++ perilous_procs/phase_2.rb
@@ -0,0 +1,52 @@
+def xnor_select(arr, prc_1, prc_2)
+  arr.select do |el| 
+    result_1, result_2 = prc_1.call(el), prc_2.call(el)
+    result_1 && result_2 || !result_1 && !result_2
+  end
+end
+
+is_even = Proc.new { |n| n % 2 == 0 }
+is_odd = Proc.new { |n| n % 2 != 0 }
+is_positive = Proc.new { |n| n > 0 }
+p xnor_select([8, 3, -4, -5], is_even, is_positive)         # [8, -5]
+p xnor_select([-7, -13, 12, 5, -10], is_even, is_positive)  # [-7, -13, 12]
+p xnor_select([-7, -13, 12, 5, -10], is_odd, is_positive)   # [5, -10]
+p "------------"
+
+
+def filter_out!(arr, &blk)
+  arr.delete_if {|el| blk.call(el)}
+end
+
+arr_1 = [10, 6, 3, 2, 5 ]
+filter_out!(arr_1) { |x| x.odd? }
+p arr_1     # [10, 6, 2]
+
+arr_2 = [1, 7, 3, 5 ]
+filter_out!(arr_2) { |x| x.odd? }
+p arr_2     # []
+
+arr_3 = [10, 6, 3, 2, 5 ]
+filter_out!(arr_3) { |x| x.even? }
+p arr_3     # [3, 5]
+
+arr_4 = [1, 7, 3, 5 ]
+filter_out!([1, 7, 3, 5 ]) { |x| x.even? }
+p arr_4 # [1, 7, 3, 5]
+p "------------"
+
+
+def multi_map(arr, n=1, &blk)
+  arr.map do |el|
+    n.times {el = blk.call(el)}
+    el
+  end
+end
+
+p multi_map(['pretty', 'cool', 'huh?']) { |s| s + '!'}      # ["pretty!", "cool!", "huh?!"]
+p multi_map(['pretty', 'cool', 'huh?'], 1) { |s| s + '!'}   # ["pretty!", "cool!", "huh?!"]
+p multi_map(['pretty', 'cool', 'huh?'], 3) { |s| s + '!'}   # ["pretty!!!", "cool!!!", "huh?!!!"]
+p multi_map([4, 3, 2, 7], 1) { |num| num * 10 }             # [40, 30, 20, 70]
+p multi_map([4, 3, 2, 7], 2) { |num| num * 10 }             # [400, 300, 200, 700]
+p multi_map([4, 3, 2, 7], 4) { |num| num * 10 }             # [40000, 30000, 20000, 70000]
+p "------------"
