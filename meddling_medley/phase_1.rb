3e42bad26006f8149747ec7835e8244d1262cba2
seanchavez <realseanchavez@gmail.com>
2021-07-02 10:33:15 -0700

    meddling_medley: Complete Phase 1

--- meddling_medley/phase_1.rb
+++ meddling_medley/phase_1.rb
@@ -57,21 +57,51 @@ p counted_characters("runtime") # []
 puts "---------"
 
 
+def triplet_true(str)
+  (str.length - 2).times {|i| return true if str[i, 3] == str[i] * 3}
+  false
+end
 
+p triplet_true('caaabb')        # true
+p triplet_true('terrrrrible')   # true
+p triplet_true('runninggg')     # true
+p triplet_true('bootcamp')      # false
+p triplet_true('e')             # false
 puts "---------"
 
 
+def energetic_encoding(str, hsh)
+  str.chars.map do |char|
+    case 
+    when hsh.has_key?(char) then hsh[char]
+    when char == " " then char
+    else "?"
+    end
+  end.join
+end
 
+p energetic_encoding('sent sea',
+    'e'=>'i', 's'=>'z', 'n'=>'m', 't'=>'p', 'a'=>'u'
+) # 'zimp ziu'
+
+p energetic_encoding('cat',
+    'a'=>'o', 'c'=>'k'
+) # 'ko?'
+
+p energetic_encoding('hello world',
+    'o'=>'i', 'l'=>'r', 'e'=>'a'
+) # '?arri ?i?r?'
+
+p energetic_encoding('bike', {}) # '????'
 puts "---------"
 
 
+def uncompress(str)
+  new_str = ""
+  str.length.times {|i| new_str += str[i-1] * str[i].to_i if i.odd?}
+  new_str
+end
 
-puts "---------"
-
-
-
-puts "---------"
-
-
-
-puts "---------"
+p uncompress('a2b4c1') # 'aabbbbc'
+p uncompress('b1o2t1') # 'boot'
+p uncompress('x3y1x2z4') # 'xxxyxxzzzz'
