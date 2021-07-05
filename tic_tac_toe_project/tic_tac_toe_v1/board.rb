6f4dbd7bd25761ee76436bb513c7dba4076eda03
seanchavez <realseanchavez@gmail.com>
2021-07-04 20:13:43 -0700

    tic_tac_toe_project: Board class WIP II

--- tic_tac_toe_project/tic_tac_toe_v1/board.rb
+++ tic_tac_toe_project/tic_tac_toe_v1/board.rb
@@ -12,9 +12,32 @@ class Board
   end
 
   def place_mark(position, mark)
-    unless self.valid?(position) && self.empty?(position)
-      raise "You can't put your mark there!"
+    if self.valid?(position) && self.empty?(position)
+      @grid[position.first][position.last] = mark
+    else
+      raise "INVALID POSITION"
     end
-    @grid[position.first][position.last] = mark
+  end
+
+  def print
+    @grid.each do |row|
+      puts row.join(' ')
+    end
+  end
+
+  def win_row?(mark)
+    @grid.any? {|row| row.all? {|m| m == mark}}
+  end
+
+  def win_col?(mark)
+    @grid.transpose..any? {|row| row.all? {|m| m == mark}}
+  end
+
+  def win_diagonal?
+    top_left = @grid[0][0]
+    bottom_left = @grid[-1][0]
+    top_to_bottom = @grid.each_with_index {|row, i| break unless row[i] == top_left}
+    bottom_to_top = @grid.reverse.each_with_index {|row, i| break unless row[i] == bottom_left}
+    top_to_bottom || bottom_to_top ? true : false
   end
 end
